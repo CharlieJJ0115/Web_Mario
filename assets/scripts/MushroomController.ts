801a628936262decfefb8d56c2aabd721eced628
Charlie <shangyuchiayi@gmail.com>
2026-05-25 18:57:15 +0800

    Mushroom basics physics and movement

---
+++ assets/scripts/MushroomController.ts
@@ -0,0 +1,136 @@
+import {
+    _decorator,
+    BoxCollider2D,
+    Camera,
+    Component,
+    ERigidBody2DType,
+    RigidBody2D,
+    Size,
+    Sprite,
+    UITransform,
+    Vec2,
+} from 'cc';
+
+const { ccclass, property } = _decorator;
+
+@ccclass('MushroomController')
+export class MushroomController extends Component {
+    @property
+    public moveSpeed = 60;
+
+    @property(Vec2)
+    public bodySize = new Vec2(16, 16);
+
+    @property
+    public destroyMargin = 64;
+
+    @property(Camera)
+    public camera: Camera | null = null;
+
+    @property
+    public fallbackDestroyY = -300;
+
+    private body: RigidBody2D | null = null;
+    private collider: BoxCollider2D | null = null;
+
+    protected onLoad(): void {
+        this.setupComponents();
+    }
+
+    protected update(): void {
+        this.applyMovement();
+        this.destroyIfOutOfFrame();
+    }
+
+    private setupComponents(): void {
+        let transform = this.node.getComponent(UITransform);
+        if (!transform) {
+            transform = this.node.addComponent(UITransform);
+        }
+        transform.setContentSize(this.bodySize.x, this.bodySize.y);
+
+        if (!this.node.getComponent(Sprite)) {
+            this.node.addComponent(Sprite);
+        }
+
+        this.body = this.node.getComponent(RigidBody2D);
+        if (!this.body) {
+            this.body = this.node.addComponent(RigidBody2D);
+        }
+        this.body.type = ERigidBody2DType.Dynamic;
+        this.body.fixedRotation = true;
+        this.body.gravityScale = 1;
+
+        this.collider = this.node.getComponent(BoxCollider2D);
+        if (!this.collider) {
+            this.collider = this.node.addComponent(BoxCollider2D);
+        }
+        this.collider.sensor = false;
+        this.collider.size = new Size(this.bodySize.x, this.bodySize.y);
+        this.collider.apply();
+    }
+
+    private applyMovement(): void {
+        if (!this.body) {
+            return;
+        }
+
+        const velocity = this.body.linearVelocity.clone();
+        velocity.x = -Math.abs(this.moveSpeed);
+        this.setBodyVelocity(velocity);
+    }
+
+    private setBodyVelocity(velocity: Vec2): void {
+        if (!this.body) {
+            return;
+        }
+
+        const bodyWithMethods = this.body as unknown as {
+            setLinearVelocity?: (value: Vec2) => void;
+            wakeUp?: () => void;
+        };
+
+        if (bodyWithMethods.setLinearVelocity) {
+            bodyWithMethods.setLinearVelocity(velocity);
+        } else {
+            this.body.linearVelocity = velocity;
+        }
+
+        bodyWithMethods.wakeUp?.();
+    }
+
+    private destroyIfOutOfFrame(): void {
+        const worldPosition = this.node.worldPosition;
+        if (worldPosition.y < this.fallbackDestroyY) {
+            this.node.destroy();
+            return;
+        }
+
+        if (!this.camera) {
+            return;
+        }
+
+        const visibleHalfSize = this.getCameraVisibleHalfSize();
+        const cameraPosition = this.camera.node.worldPosition;
+        const minVisibleX = cameraPosition.x - visibleHalfSize.x - this.destroyMargin;
+        const minVisibleY = cameraPosition.y - visibleHalfSize.y - this.destroyMargin;
+
+        if (worldPosition.x < minVisibleX || worldPosition.y < minVisibleY) {
+            this.node.destroy();
+        }
+    }
+
+    private getCameraVisibleHalfSize(): Vec2 {
+        if (!this.camera) {
+            return new Vec2(480, 320);
+        }
+
+        const halfHeight = this.camera.orthoHeight;
+        const canvasTransform = this.camera.node.parent?.getComponent(UITransform);
+        const aspect = canvasTransform && canvasTransform.height !== 0
+            ? canvasTransform.width / canvasTransform.height
+            : 16 / 9;
+
+        return new Vec2(halfHeight * aspect, halfHeight);
+    }
+}
